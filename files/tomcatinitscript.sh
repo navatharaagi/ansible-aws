0d6634280e8d14d55197b1d6db33face99499965
Navatha Raagi <navatharaagi@gmail.com>
2018-05-08 14:34:55 -0700

    Updated tomcatinitscript.sh.

--- files/tomcatinitscript.sh
+++ files/tomcatinitscript.sh
@@ -49,9 +49,9 @@ start() {
   else
     #Start tomcat
     echo -e "\e[00;32mStarting tomcat\e[00m"
-    # ulimit -n 100000
-    # umask 007
-    # /bin/su -p -s /bin/sh tomcat
+    #ulimit -n 100000
+    #umask 007
+    #/bin/su -p -s /bin/sh tomcat
         if [ `user_exists $TOMCAT_USER` = "1" ]
         then
                 su $TOMCAT_USER -c $CATALINA_HOME/bin/startup.sh
