c8f75a06d9830f275b4a09b079e42891bbd87f4f
Navatha Raagi <navatharaagi@gmail.com>
2018-05-11 11:24:53 -0700

    Updated tomcatinitscript.sh.

--- files/tomcatinitscript.sh
+++ files/tomcatinitscript.sh
@@ -3,8 +3,8 @@
 # chkconfig: 345 99 28
 # description: Starts/Stops Apache Tomcat
 #
-# Tomcat 7 start/stop/status script
-# Forked from: https://gist.github.com/valotas/1000094
+#Tomcat 7 start/stop/status script
+#Forked from: https://gist.github.com/valotas/1000094
 #@author: Miglen Evlogiev <bash@miglen.com>
 #
 #Release updates:
