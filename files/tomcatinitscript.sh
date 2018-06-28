91e2f7982205b80ca7f77c5ca9635d0fd983fc2b
Navatha Raagi <navatharaagi@gmail.com>
2018-06-28 09:50:20 -0700

    Updated tomcatinitscript.sh.

--- files/tomcatinitscript.sh
+++ files/tomcatinitscript.sh
@@ -22,10 +22,10 @@ export JAVA_HOME=/opt/oracle/jdk1.8.0_131
 #  export PATH=$JAVA_HOME/bin:$PATH
 export PATH=$PATH:/opt/oracle/jdk1.8.0_131/bin:/opt/oracle/jdk1.8.0_131/jre/bin
 
-#  CATALINA_HOME is the location of the bin files of Tomcat
+# CATALINA_HOME is the location of the bin files of Tomcat
 export CATALINA_HOME=/usr/share/tomcat
 
-#  CATALINA_BASE is the location of the configuration files of this instance of Tomcat
+# CATALINA_BASE is the location of the configuration files of this instance of Tomcat
 export CATALINA_BASE=/usr/share/tomcat
 
 # TOMCAT_USER is the default user of tomcat
