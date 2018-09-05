a0ae3f5b983f96f7f99cdeb3ef92d8af80b0e20b
Navatha Raagi <navatharaagi@gmail.com>
2018-09-05 11:08:25 -0700

    Updated tomcatinitscript.sh.

--- files/tomcatinitscript.sh
+++ files/tomcatinitscript.sh
@@ -8,8 +8,8 @@
 # @author: Miglen Evlogiev <bash@miglen.com>
 #
 # Release updates:
-#  Updated method for gathering pid of the current proccess
-#  Added usage of CATALINA_BASE
+# Updated method for gathering pid of the current proccess
+# Added usage of CATALINA_BASE
 #  Added coloring and additional status
 #  Added check for existence of the tomcat user
 #
